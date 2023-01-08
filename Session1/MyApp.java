657e477c1ffe6fc518c315e49b702894a23d6b04
Mrigank Shashi <shashimrigank@gmail.com>
2023-01-08 21:06:17 +0530

    Day 1 java fundamentals

---
+++ Session1/MyApp.java
@@ -0,0 +1,24 @@
+// In java program is always a class
+public class MyApp {
+	
+	// 1 class in the project has main method
+	// i.e the class from where execution begins
+	public static void main(String[] args) {
+		// Anything you write in main
+		// Will be executed in sequence, i.e line by line
+		
+		System.out.println("Be Exceptional");
+		System.out.println("Work Hard, But Smartly :)");
+		System.out.println("Search the candle, rather than cursing the darkness !!");
+
+	}
+
+}
+class Mango{
+	
+}
+
+class Lunchbreak{
+	
+}
+// 3:42 timings
