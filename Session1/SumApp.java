14e4e7e87112f2f7fea20f570146005941abae47
Mrigank Shashi <shashimrigank@gmail.com>
2023-01-09 20:58:36 +0530

    Day 1 Fundamentals

---
+++ Session1/SumApp.java
@@ -0,0 +1,11 @@
+
+public class SumApp {
+	public static void main(String[]args) {
+		int x = 20;
+		int y = 40;
+		int sum = x + y;
+		System.out.println(" The value of x is :"+ x);
+		System.out.println(" The value of y is :"+ y);
+		System.out.println(" Sum of x and y is : "+ sum);
+	}
+}
