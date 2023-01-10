769f53e929bd81f28a46d0ed717a76628b2bc09a
Mrigank Shashi <shashimrigank@gmail.com>
2023-01-10 22:03:59 +0530

    Day 1 Java Fundamentals

---
+++ Session1/SubApp.java
@@ -0,0 +1,13 @@
+
+public class SubApp {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 15;
+		int subtract = a - b;
+		System.out.println("The value of a is:"+ a);
+		System.out.println("The value of b is:"+ b);
+		System.out.println("The subtracted value for a and b is:"+ subtract);
+	}
+
+}
