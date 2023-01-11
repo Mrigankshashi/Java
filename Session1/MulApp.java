59c53086664c49939fe13eace7b7da0491e18de7
Mrigank Shashi <shashimrigank@gmail.com>
2023-01-11 21:40:38 +0530

    Day 1 Java Fundamentals

---
+++ Session1/MulApp.java
@@ -0,0 +1,13 @@
+
+public class MulApp {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 34;
+		int mul = (a*b);
+		System.out.println("The value of a is: "+ a);
+		System.out.println("The value of b is: "+ b);
+		System.out.println("The value of a multiply by b is: "+ mul);
+	}
+
+}
