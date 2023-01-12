3ee0e93e0c04ebd446ecc77e7c853034f8e7fedb
Mrigank Shashi <shashimrigank@gmail.com>
2023-01-12 22:19:46 +0530

    Day1 java Fundamentals

---
+++ Session1/DivApp.java
@@ -0,0 +1,14 @@
+
+public class DivApp {
+
+	public static void main(String[] args) {
+		int x1 = 35;
+		int x2 = 5;
+		int Div = x1/x2;
+		System.out.println("The Value of x1 is: "+x1);
+		System.out.println("The value of x2 is: "+x2);
+		System.out.println("The Division of x1 and x2 is: "+Div);
+
+	}
+
+}
